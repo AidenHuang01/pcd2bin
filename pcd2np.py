8bf7bd21af306d604aceb732b8aa4c9c97438d41
Yucheng Huang <yuh032@ucsd.edu>
2022-07-08 11:29:27 -0700

    auto commit

--- pcd2np.py
+++ pcd2np.py
@@ -1,12 +1,19 @@
 import numpy as np 
 import open3d as o3d
+import os
+from tqdm import tqdm
 
-pcd = o3d.io.read_point_cloud("./0038.pcd")
-out_arr = np.asarray(pcd.points)  
-num_points = out_arr.shape[0]
-intensity = np.zeros((num_points, 1))
-out_arr = np.hstack((out_arr, intensity))
-out_arr[:,0] = -out_arr[:,0]
-print("output array from input list : ", out_arr)  
-print("out_arr.shape: ", out_arr.shape)
-np.save('0038_flip_x.npy', out_arr) 
+parent_path = './test04_17_wild6/mtlb-pcdFromBag/'
+for file in tqdm(os.listdir(parent_path)):
+    if file[-4:] != '.pcd':
+        continue
+    pcd_path = parent_path + file
+    pcd = o3d.io.read_point_cloud(pcd_path)
+    out_arr = np.asarray(pcd.points)  
+    num_points = out_arr.shape[0]
+    intensity = np.zeros((num_points, 1))
+    out_arr = np.hstack((out_arr, intensity))
+    out_arr[:,0] = -out_arr[:,0]
+    output_file = file[:-4] + '.npy'
+    np.save(output_file, out_arr) 
+    # print('successfully converted ', output_file)
